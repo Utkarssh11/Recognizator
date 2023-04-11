d66d8b5fa5098990326f31430d0b42944f932e00
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-04-11 18:50:03 +0530

    Add files via upload

---
+++ Recognizator/main.py
@@ -0,0 +1,9 @@
+name = input ('hello , what is your name? ')
+print ('Hello ' + name + ', now i know you better ')
+surname = input ('what about your surname ? ')
+print ('Oh , hello ' + name + ' '+  surname +' nice to met you')
+year_Birth = int(input ('what about your age ? Just tell to me your year of birth '))
+age = 2021 - year_Birth
+print ('NAME: ' + name )
+print ('SURNAME: ' + surname)
+print ('AGE: ' + str(age))
